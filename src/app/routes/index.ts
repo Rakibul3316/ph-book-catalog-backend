dcd0410b828a16ff7ca6023cf46458568c35203c
Rakibul3316 <service.rakibul4341@gmail.com>
2023-07-14 20:34:48 +0600

    add auth & user route

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,11 +1,21 @@
 import express from 'express';
 import { BookRoutes } from '../modules/book/book.route';
+import { UserRoutes } from '../modules/user/user.route';
+import { AuthRoutes } from '../modules/auth/auth.route';
 const router = express.Router()
 
 const moduleRoutes = [
   {
     path: "/books",
     route: BookRoutes,
+  },
+  {
+    path: "/auth",
+    route: AuthRoutes,
+  },
+  {
+    path: "/users",
+    route: UserRoutes,
   },
 ];
 
